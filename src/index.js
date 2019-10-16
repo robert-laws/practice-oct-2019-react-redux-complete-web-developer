522a858d750abcf43871d6ad6ccd7741e903f674
Robert Laws <roblaws@me.com>
2019-10-16 13:03:51 +0300

    add redux-logger middleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './redux/root-reducer'
 
@@ -8,10 +9,12 @@ import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const store = createStore(rootReducer)
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>, 
   document.getElementById('root'));
+  
